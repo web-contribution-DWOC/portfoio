bdeb5b1f58252eee9a435f2ab425e48b31f5a4e9
Naresh <chandanbatven@gmail.com>
2022-01-25 14:02:34 +0530

    improve

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 var fab2 = document.getElementById("fab2");
+var home = document.getElementById("home");
+var stack = document.getElementById("stack");
+var projects = document.getElementById("projects");
 
 function myScroll() {
     var y = window.scrollY;
@@ -10,16 +13,43 @@ function myScroll() {
         fab2.style.display = "none";
     }
 }
-var audio = new Audio("sound/tap.wav");
+
+
+/**
+ * music list
+ */
+var audio = new Audio("sound/slide.wav");
+var back = new Audio("sound/backmusic.mp3");
+var tap = new Audio("sound/tap.wav");
+
+function tapSound() {
+    tap.currentTime=0;
+    tap.play();      
+}
+
+function tapStop() {
+
+    tap.pause();    
+}
+
 
 function play() {
     audio.currentTime = 0;
     audio.play();
 }
+function backmusic() {
+   
+    back.currentTime = 0;
+    back.play();
+}
 
 
 
-fab2.addEventListener("click", play)
 
-
+fab2.addEventListener("click", play);
+home.addEventListener("click", play);
+stack.addEventListener("click", play);
+projects.addEventListener("click", play);
 window.addEventListener("scroll", myScroll)
+//window.addEventListener("mousemove", backmusic);
+//document.getElementById('MyAudioElement').play()
